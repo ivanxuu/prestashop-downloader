04643f07bd2c10819731a01650af3f8ccf84dba5
Ivan H <ivan@drails.com>
2019-06-23 10:14:36 +0200

    Include other info like variants and images
    The code has been update so now it:
    
    - Includes all variants(combinations) of a product.
    - Give all images for each variant.
    - Tells if the product is active or not.
    
    The image links now are returned as a relative link. Ex:
    Instead of `https://www.tiendaprestashopejemplo.es/blouses/blouse` will return `/blouses/blouse`.

--- README.md
+++ README.md
@@ -14,9 +14,8 @@ Volcar la base de datos de todos los productos (o uno en particular) a un ficher
 
 ## NOTAS Y ADVERTENCIAS
  - Este fichero espera ser colocado en la **raiz de prestashop**.  El motivo es para que sepa encontrar correctamente el fichero donde estan los datos de la base de datos (`'app/config/parameters.php'`). En su defecto dará error indicando la variable que falta. Tendrás seguramente que ponerla manualmente modificando este codigo: Ej `$database_host= 'localhost';`
- - Si deseas cambiar los datos de los productos que se van a recuperar, tendrás que modificar la cadena SQL donde se solicita los productos.  O hacer algun tipo de query del estilo `select * FROM ...`
+ - Si deseas cambiar los datos (por ejemplo añadir alguna información de otra tabla o similar) de los productos que se van a recuperar, tendrás que modificar la cadena SQL donde se solicita los productos.  O hacer algun tipo de query del estilo `select * FROM ...`
  - Lo he probado en la última versión de prestashop 1.7 y 1.6. Hay un fichero PHP por cada versión.
- - La URL de las imagenes no sale entera en la base de datos, por lo que en la query esta escrito a pelo el dominio de la tienda (Busca la cadena `https://www.tiendaprestashopejemplo.es/`). La verdad que no tengo claro de donde se recupera el dominio de la tienda para haberlo hecho automático. Por lo que **habrá que modificar la URL de la tienda en el fichero!**
  - **En caso de ser otro prefijo distinto a `ps_` habra que modificarlo!**. Podría pasar que tengan un prefijo diferente a `ps_`. Por defecto es `ps_`, pero si lo cambiaron cuando hicieron la instalación de prestashop, pues habrá que modificar la query.
  - Dentro del script, hay varias lineas comentadas que pueden ser utiles para debuggear algo que no funcione.
 
--- descargar-1.6.php
+++ descargar-1.6.php
@@ -90,27 +90,28 @@ $query    = 'SELECT DISTINCT
     ps_product.reference AS \'product_reference\',
     ps_stock_available.quantity AS \'available_stock\',
     ps_manufacturer.name AS \'brand\',
+    ps_product.active AS \'active\',
     ps_product_lang.description AS \'description\',
     ps_product_lang.name AS \'title\',
-    ps_product.active AS \'active\',
-    concat("https://www.tiendaprestashopejemplo.es/", ps_category_lang.link_rewrite, "/", ps_product_lang.link_rewrite) AS \'deeplink\',
-    concat("https://www.tiendaprestashopejemplo.es/", ps_image.id_image, "-large_default/", ps_product_lang.link_rewrite, ".jpg") AS \'imagelink\'
+    ps_product_lang.link_rewrite AS \'productlangrewrite\',
+    concat("/", ps_category_lang.link_rewrite, "/", ps_product_lang.link_rewrite) AS \'deeplink\',
+    concat("/", ps_image.id_image, "-large_default/", ps_product_lang.link_rewrite, ".jpg") AS \'imagelink\'
 FROM
     ps_product
-    INNER JOIN
-        ps_stock_available
-        ON ps_stock_available.id_product = ps_product.id_product
-    INNER JOIN
-        ps_manufacturer
-        ON ps_manufacturer.id_manufacturer = ps_product.id_manufacturer
-    INNER JOIN
-        ps_product_lang
-        ON ps_product_lang.id_product = ps_product.id_product
-    INNER JOIN
-        ps_category_lang
-        ON ps_category_lang.id_category = ps_product.id_category_default
-    INNER JOIN
-        ps_image
+INNER JOIN
+    ps_stock_available
+    ON ps_stock_available.id_product = ps_product.id_product
+INNER JOIN
+    ps_manufacturer
+    ON ps_manufacturer.id_manufacturer = ps_product.id_manufacturer
+INNER JOIN
+    ps_product_lang
+    ON ps_product_lang.id_product = ps_product.id_product
+INNER JOIN
+    ps_category_lang
+    ON ps_category_lang.id_category = ps_product.id_category_default
+INNER JOIN
+    ps_image
 	ON ps_image.id_product = ps_product.id_product
 '. $where .'
 GROUP BY
@@ -119,28 +120,75 @@ $dbresult = mysqli_query($dbconnect, $query);
 
 #// Test para ver que es lo que devuelve la query
 #if (mysqli_num_rows($dbresult) > 0) {
-#  while($row = mysqli_fetch_assoc($dbresult)) {
-#    //echo $row["product_id"];
-#    echo implode($row);
+#  while($product_row = mysqli_fetch_assoc($dbresult)) {
+#    foreach($product_row as $key => $value)
+#    {
+#      echo $key." = ". $value."</br>";
+#    }
 #  }
-#
 #} else {
 #  echo "0 results";
 #}
 #exit;
+
 
 // Create a new XML document
 $doc = new DomDocument('1.0');
 // create root node
 $root = $doc->createElement('root');
 $root = $doc->appendChild($root);
-while ($row = mysqli_fetch_assoc($dbresult)) {
-  $newRow = $doc->createElement('product');
-  foreach ( $row as $name=>$value )    {
+while ($product_row = mysqli_fetch_assoc($dbresult)) {
+  $product_id = $product_row["product_id"]; //Ex: 1
+  $product_lang_rewrite = $product_row["productlangrewrite"]; //Ex: 'faded-short-sleeves-tshirt'
+  $product = $doc->createElement('product');
+  foreach ($product_row as $name=>$value )    {
     $element = $doc->createElement($name, htmlspecialchars($value));
-    $newRow->appendChild($element);
+    $product->appendChild($element);
   }
-  $root->appendChild($newRow);
+  // Variants
+  $variants = $doc->createElement('variants');
+  $variant_query = "SELECT id_product_attribute, reference, price, unit_price_impact, weight FROM ps_product_attribute WHERE ps_product_attribute.id_product = $product_id";
+  $dbvariantresult = mysqli_query($dbconnect, $variant_query);
+  while ($variant_row = mysqli_fetch_assoc($dbvariantresult)) {
+    $variant = $doc->createElement('variant');
+    { //ALL attributes of this variant
+      $variant_id = $variant_row['id_product_attribute'];
+      //variant name
+      $query =  "SELECT GROUP_CONCAT(name SEPARATOR ' - ') AS combination FROM ps_attribute_lang WHERE id_lang=(SELECT id_lang FROM ps_lang WHERE iso_code = 'es') AND id_attribute IN (SELECT id_attribute FROM ps_product_attribute_combination WHERE id_product_attribute = $variant_id)";
+      $name_query = mysqli_query($dbconnect, $query);
+      $name_row = mysqli_fetch_assoc($name_query);
+      $variant_name = $doc->createElement('name', $name_row['combination']);
+      $variant->appendChild($variant_name);
+      //images
+      $images = $doc->createElement('images');
+      { // ALL images of this variant
+        $img_query = "SELECT
+            id_image,
+            concat(\"/\", id_image, \"-large_default/\", '$product_lang_rewrite', \".jpg\") AS imagelink,
+            concat(\"/img/p/\", id_image, \"/\", id_image, \"-large_default\", \".jpg\") AS imagelegacylink
+          FROM ps_product_attribute_image WHERE id_product_attribute = $variant_id";
+        $dbimgresult = mysqli_query($dbconnect, $img_query);
+        while ($image_row = mysqli_fetch_assoc($dbimgresult)) {
+          $image = $doc->createElement('image');
+          foreach ($image_row as $name=>$value )    {
+            $element = $doc->createElement($name, htmlspecialchars($value));
+            $image->appendChild($element);
+          }
+          $images->appendChild($image);
+        }
+      }
+      $variant->appendChild($images);
+      //rest of values of product
+      foreach ( $variant_row as $v_name=>$v_value )    {
+        $v_element = $doc->createElement($v_name, htmlspecialchars($v_value));
+        $variant->appendChild($v_element);
+      }
+    }
+    $variants->appendChild($variant);
+  }
+  $product->appendChild($variants);
+
+  $root->appendChild($product);
 }
 $xml_string = $doc->saveXML();
 header('Content-type: application/xml');
